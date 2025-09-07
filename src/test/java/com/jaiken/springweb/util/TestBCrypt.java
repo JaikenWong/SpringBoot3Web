35306d331fcb40d1bf9afaf8328313f1114eeebf
JaikenWong <Jiccence@gmail.com>
2025-09-07 17:05:30 +0800

    添加密码测试

--- src/test/java/com/jaiken/springweb/util/TestBCrypt.java
+++ src/test/java/com/jaiken/springweb/util/TestBCrypt.java
@@ -1,12 +1,20 @@
 package com.jaiken.springweb.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @Description
  * @Created WangJingshen
  * @Date 2025/8/25 15:03
  */
+@Slf4j
 public class TestBCrypt {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
@@ -16,5 +24,9 @@ public class TestBCrypt {
         System.out.println("Encoded Password: " + encodedPassword2);
         boolean matches = encoder.matches(rawPassword, encodedPassword);
         System.out.println("密码匹配结果: " + matches);
+        List<Map<String,List<String>>> data1 = new ArrayList<>();
+        data1.add(Map.of("password", List.of(encodedPassword)));
+        List<Map<String,List<PasswordEncoder>>> data2 = new ArrayList<>();
+        data2.add(Map.of("password", List.of(encoder)));
     }
 }
